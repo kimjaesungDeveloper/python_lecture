44759276a69e0f05a587de5a426bbc58e4737cf6
unknown <kimjaesung145@gmail.com>
2026-01-15 11:19:01 +0900

    parent-chile init 호출

--- 20260115_python_exam_11.py
+++ 20260115_python_exam_11.py
@@ -256,6 +256,82 @@ Paul.hi()
 Paul.hello()
 Paul.study()
 
+
+## 클래스 포함관계
+
+# 포함관계
+# 자동차 클래스가 엔진 클래스의 인스턴스를 포함하는 것을 생각
+# 자동차는 엔진 없이는 작동x, 자동차 클래스는 엔진 클래스의 인스턴스를 내부에 포함
+# 포함관계에서는 클래스 간에 부모 자식이라는 개념X
+# 대신에 한 클래스가 다른 클래스의 인스턴스를 포함하는 관계
+
+
+class Person:
+    def hi(self):
+        print("안녕")
+
+
+class Add_Person:
+    def __init__(self):
+        self.person_directiory = []  # 사람 인명부에 사람을 append 한다.
+
+    def add_person(self, person):
+        self.person_directiory.append(person)
+
+
+# Person 객체 생성
+person1 = Person()
+person2 = Person()
+
+add_person_instance = Add_Person()
+
+add_person_instance.add_person(person1)
+add_person_instance.add_person(person2)
+
+for person in add_person_instance.person_directiory:
+    person.hi()
+
+
+## 자식 - 부모 클래스의 생성자 호출
+
+
+class Person:
+    def __init__(self):
+        print("Person __init__ 호출")
+        self.hello = "안녕"
+
+
+class Student(Person):
+    def __init__(self):
+        print("Student __init__ 호출")
+        super().__init__()  # 부모의 생성자를 호출한다.
+        self.classroom = "A class"
+
+
+Jin = Student()
+print(Jin.classroom)
+print(Jin.hello)
+
+# 자식클래스에 __init__ 생성자가 있다면 super().__init__() 써줘야된다.(안써주게 되면 자동으로 호출x)
+# 자식클래스에 __init__ 생성자가 없다면 super().__init__() 생략가능(자동으로 호출)
+
+
+class Person2:
+    def __init__(self):
+        print("Person init 호출")
+        self.hello = "안녕"
+
+    def print_hi(self):
+        print("안녕하세요")
+
+
+class Student2(Person2):
+    pass
+
+
+Jin2 = Student2()
+
+
 # ==========================================================================
 # 개별 class 이론 추가 내용
 
