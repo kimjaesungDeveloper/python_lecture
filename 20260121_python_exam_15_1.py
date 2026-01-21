001da50de352e3d55c3f1349cca63fcfeb0ebee6
unknown <kimjaesung145@gmail.com>
2026-01-21 11:05:34 +0900

    numpy 인덱싱 슬라이싱 내용추가

--- 20260121_python_exam_15_1.py
+++ 20260121_python_exam_15_1.py
@@ -167,3 +167,48 @@ arr3
 # # -100 ~ 100 사이에 있는 정수로 2 * 3 행렬 생성
 
 # print(random_arr)
+
+# 인덱싱
+#  인덱싱으로 [:] 할때  예시로 0:3 이라 한다 하면 <3 까지로 0,1,2 인덱스까지만 된다.
+
+# 인덱싱
+
+li = [1, 2, 3, 4]
+print(li[1])
+print(li[0:2])
+
+# 1차원 배열의 인덱싱
+arr1 = np.array([1, 2, 3, 4])
+print(arr1[0])  # 한개접근
+print("1차원 배열 인덱싱")
+# arr1[시작 : 끝 : 보폭]
+print(arr1[0:3:2])
+print(arr1[0::])
+print(arr1[:2:])
+
+# 2차원 배열 인덱싱
+arr2 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+print(arr2)
+# [[1 2 3 4]
+#  [5 6 7 8]]
+print(arr2[0, 1])
+print(arr2[1, 1])
+print(arr2[::, ::])
+print(arr2[0:2:, 1:3:])
+
+# fancy 인덱싱 => 원하는 인덱스를 배열로 넘겨준다
+arr1 = np.array([10, 20, 30, 40, 50])
+print(arr1)
+print(arr1[[0, 1, 3]])
+
+arr2 = np.array([[10, 20, 30, 40], [50, 60, 70, 80]])
+# print(arr2)
+print(arr2[[0, 1], [1, 3]])
+
+# 논리값 인덱싱
+arr1 = np.array([1, 2, 3, 4])
+arr1[[True, False, True, False]]
+# print(arr1)
+# print(arr1[[True, False, True, False]])
+arr4 = arr1 > 2
+print(arr4)
